d394ab3927fd1efeafe1d63be114dd0f6354ad1c
mattis1234567 <mattisvrager@gmail.com>
2025-11-13 07:18:07 +0200

    Added the main body to add post and login pages

--- vue-wad/src/views/AddPostView.vue
+++ vue-wad/src/views/AddPostView.vue
@@ -1,8 +1,13 @@
 <template>
     <Header></Header>
-    <div>
-        <h1>This is add post view</h1>
-    </div>
+    <main>
+        <form action="index.html">
+            <textarea name="" id="fpostbody" required placeholder="What do you want to write about?"></textarea> <br>
+            <label for="fselectedfile" class="file-link">Attach an image</label>
+            <input type="file" name="fselectedfile" id="fselectedfile" accept="image/png, image/jpeg" class="file-input"><br>
+            <input type="submit" value="Create post" id="fsubmit">
+        </form>
+    </main>
     <Footer></Footer>
 </template>
 <script>
@@ -17,3 +22,47 @@ export default {
     }
 }
 </script>
+<style>
+* {
+	font-family: sans-serif;
+	box-sizing: border-box;
+}
+body {
+	text-align: center;
+	background-color: #eeeded;
+	max-width: min(100%, clamp(600px, 60vw, 1200px));
+	margin: 0 auto;
+	padding: 1em;
+}
+
+main {
+	margin: 0 auto;
+	padding: 1em;
+}
+form {
+	width: 100%;
+	padding: 1em;
+	margin: 0em;
+	background-color: var(--background-color);
+	border-radius: 10px;
+}
+
+form textarea {
+    width: 100%;
+    height: 25vh;
+    padding: 1em;
+    resize: none;
+}
+
+.file-input {
+	display: none;
+}
+
+.file-link {
+	display: inline-block;
+	margin: 1em 0;
+	color: teal;
+	text-decoration: underline;
+	cursor: pointer;
+}
+</style>
--- vue-wad/src/views/SignUpView.vue
+++ vue-wad/src/views/SignUpView.vue
@@ -1,8 +1,20 @@
 <template>
     <Header></Header>
-    <div>
-        <h1>This is signup view</h1>
-    </div>
+    <main>
+        <form action="index.html">
+            <div class="form-title">
+                <h1>Welcome to PostIt</h1>
+                <a href="">Create an account</a>
+                <p>or<br>Please log in</p>
+            </div>
+            <input type="email" name="email" id="femail" placeholder="Email" required> <br>
+            <input type="password" name="password" id="fpass" placeholder="Password" required> <br>
+            <input type="submit" value="Log in"> <br>
+            <div class="form-forgot-password">
+                <a href="">Forget password</a>
+            </div>
+        </form>
+    </main>
     <Footer></Footer>
 </template>
 <script>
@@ -17,3 +29,53 @@ export default {
     }
 }
 </script>
+<style>
+* {
+	font-family: sans-serif;
+	box-sizing: border-box;
+}
+body {
+	text-align: center;
+	background-color: #eeeded;
+	max-width: min(100%, clamp(600px, 60vw, 1200px));
+	margin: 0 auto;
+	padding: 1em;
+}
+
+main {
+	margin: 0 auto;
+	padding: 1em;
+}
+form input[type="submit"], form input[type="file"] {
+	margin-top: 0em;
+}
+
+form input[type="email"], form input[type="password"] {
+	margin: 0.2em 0;
+	width: 100%;
+	line-height: 32px;
+}
+
+form input[type="submit"] {
+	margin-top: 0.5em;
+    background-color: var(--primary-color);
+	width: 100%;
+	padding: 0.75em;
+	font-size: 1rem;
+	border: none;
+	border-radius: 8px;
+	transition: background-color 0.2s linear;
+}
+
+form input[type="submit"]:hover {
+	background-color: var(--primary-color-hover);
+}
+
+form a {
+	color: teal;
+}
+
+.form-forgot-password {
+	margin-top: 1em;
+}
+</style>
